75483d91975cbb479830cd3b0d25c3639bd1fc08
toitek <issawandhe@gmail.com>
2022-05-21 23:19:07 +0300

    error fix

--- fill_hex_array.c
+++ fill_hex_array.c
@@ -1,33 +1,33 @@
 #include "main.h"
 /**
- * fill_hex_array - writes the character c to stdout
+ * fill_hexadecimal_array - writes the character c to stdout
  *
- * @bnr: array where is stored the binary.
- * @hex: array where is stored the hexadecimal.
- * @isupp: integer that determines if the hexadecimal array is
+ * @binary: array where is stored the binary.
+ * @hexadecimal: array where is stored the hexadecimaladecimal.
+ * @is_uppercase: integer that determines if the hexadecimaladecimal array is
  * in uppercase or lowercase letter.
- * @limit: size of hex
+ * @limit: size of hexadecimal
  * Return: binary array.
  * designed by WANDHE and NGANGA
  */
-char *fill_hex_array(char *bnr, char *hex, int isupp, int limit)
+char *fill_hexadecimal_array(char *binary, char *hexadecimal, int is_uppercase, int limit)
 {
 	int op, i, j, toletter;
 
-	hex[limit] = '\0';
-	if (isupp)
+	hexadecimal[limit] = '\0';
+	if (is_uppercase)
 		toletter = 55;
 	else
 		toletter = 87;
 	for (i = (limit * 4) - 1; i >= 0; i--)
 	{
 		for (op = 0, j = 1; j <= 8; j *= 2, i--)
-			op = ((bnr[i] - '0') * j) + op;
+			op = ((binary[i] - '0') * j) + op;
 		i++;
 		if (op < 10)
-			hex[i / 4] = op + 48;
+			hexadecimal[i / 4] = op + 48;
 		else
-			hex[i / 4] = op + toletter;
+			hexadecimal[i / 4] = op + toletter;
 	}
-	return (hex);
+	return (hexadecimal);
 }
