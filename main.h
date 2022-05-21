bde08eb36f65844ebed0d7180644673af7289697
Sylvester254 <slynganga59@gmail.com>
2022-05-22 01:24:47 +0300

    main.h

--- main.h
+++ main.h
@@ -52,17 +52,14 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_function(const char *s, int ind);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
-char *octal_array(char *bnr, char *oct);
-char *long_octal_array(char *bnr, char *oct);
-char *short_octal_array(char *bnr, char *oct);
-char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
 char *binary_array(char *binary, long int int_in, int isneg, int limit);
 char *octal_array(char *bnr, char *oct);
 char *long_octal_array(char *bnr, char *oct);
 char *short_octal_array(char *bnr, char *oct);
 char *hex_array(char *bnr, char *hex, int isupp, int limit);
-=======
->>>>>>> 231b665c618ac604b865b4f488f46cb2109d3f7c
-
+char *binary_array(char *binary, long int int_in, int isneg, int limit);
+char *octal_array(char *bnr, char *oct);
+char *long_octal_array(char *bnr, char *oct);
+char *short_octal_array(char *bnr, char *oct);
+char *hex_array(char *bnr, char *hex, int isupp, int limit);
 #endif
