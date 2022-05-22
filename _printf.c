71d96db6f46dd4c848623ef1cd875ae2b440aea0
toitek <issawandhe@gmail.com>
2022-05-22 03:08:59 +0300

    renamed ibufff to ibuff

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibufff = 0;
+	unsigned int i = 0, len = 0, ibuff = 0;
 	va_list arguments;
 	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
@@ -25,7 +25,7 @@ int _printf(const char *format, ...)
 		{
 			if (format[i + 1] == '\0')
 			{
-				buffer_print(buffer, ibufff), free(buffer), va_end(arguments);
+				buffer_print(buffer, ibuff), free(buffer), va_end(arguments);
 				return (-1);
 			}
 			else
@@ -35,21 +35,21 @@ int _printf(const char *format, ...)
 				{
 					if (format[i + 1] == ' ' && !format[i + 2])
 						return (-1);
-					buffer_handler(buffer, format[i], ibufff), len++, i--;
+					buffer_handler(buffer, format[i], ibuff), len++, i--;
 				}
 				else
 				{
-					len += function(arguments, buffer, ibufff);
+					len += function(arguments, buffer, ibuff);
 					i += iden_return_function(format, i + 1);
 				}
 			}
 			i++;
 		}
 		else
-			buffer_handler(buffer, format[i], ibufff), len++;
-		for (ibufff = len; ibufff > 1024; ibufff -= 1024)
+			buffer_handler(buffer, format[i], ibuff), len++;
+		for (ibuff = len; ibuff > 1024; ibuff -= 1024)
 			;
 	}
-	buffer_print(buffer, ibufff), free(buffer), va_end(arguments);
+	buffer_print(buffer, ibuff), free(buffer), va_end(arguments);
 	return (len);
 }
